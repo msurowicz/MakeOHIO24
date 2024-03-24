de9d7850962bdd4f8a3b2d6fc64af0d0726fc9c2
msuro <msurowicz1@gmail.com>
2024-03-23 23:54:50 -0400

    fix joystick button

--- ardougne/src/main.cpp
+++ ardougne/src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 
-#define VRX_PIN  A0 // Arduino pin connected to VRX pin
-#define VRY_PIN  A1 // Arduino pin connected to VRY pin
+#define VRX_PIN A0 // Arduino pin connected to VRX pin
+#define VRY_PIN A1 // Arduino pin connected to VRY pin
 
-#define swPin A2
+// #define swPin A2
 #define xStepPin 2
 #define xDirPin 5
 #define statusLight 6
 #define yStepPin 7
 #define yDirPin 9
-// #define swPin 4
-#define fineSwitch D3
+#define swPin 4
+#define fineSwitch 3
 int j = 0;
 bool singleStep = true;
 bool step = false;
@@ -18,11 +18,11 @@ bool step = false;
 #define counterClockwise HIGH
 #define clockwise LOW
 
-
 int xValue = 0; // To store value of the X axis
 int yValue = 0; // To store value of the Y axis
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(xStepPin, OUTPUT); // sets the digital pin 13 as output
@@ -30,137 +30,157 @@ void setup() {
   pinMode(yStepPin, OUTPUT); // sets the digital pin 13 as output
   pinMode(yDirPin, OUTPUT);
   pinMode(statusLight, OUTPUT);
-  digitalWrite(statusLight,LOW);
+  digitalWrite(statusLight, LOW);
+  pinMode(swPin, INPUT_PULLUP);
 }
 
-void loop() {
-    // read analog X and Y analog values
-    xValue = analogRead(VRX_PIN);
-    yValue = analogRead(VRY_PIN);
-    bool readState = analogRead(swPin) == LOW;
-    
-    //Serial.println(readState);
-    if (readState) {
-        while(analogRead(swPin) != HIGH) {
-            ;
-        }
-        delay(600);
-        //erial.println("pressed");
-        singleStep = !singleStep;
+void loop()
+{
+  // read analog X and Y analog values
+  xValue = analogRead(VRX_PIN);
+  yValue = analogRead(VRY_PIN);
+  bool readState = digitalRead(swPin) != HIGH;
+  // Serial.println(digitalRead(fineSwitch));
+  //   Serial.println(readState);
+  // Serial.println(digitalRead(swPin));
+  if (readState)
+  {
+    while (digitalRead(swPin) != HIGH)
+    {
+      // Serial.println(digitalRead(swPin));
+    }
+    delay(600);
+    singleStep = !singleStep;
+  }
 
+  // singleStep = digitalRead(fineSwitch) == HIGH;
+
+  if (singleStep)
+  {
+    digitalWrite(statusLight, HIGH);
+  }
+  else
+  {
+    digitalWrite(statusLight, LOW);
+  }
+
+  // print data to Serial Monitor on Arduino IDE
+  // Serial.print("x = ");
+  // Serial.print(xValue);
+  // Serial.print(", y = ");
+  // Serial.println(yValue);
+  // delay(200);
+
+  int deltaX = xValue - 512;
+  int deltaY = yValue - 512;
+
+  float xPercent = deltaX / 512.0;
+  float yPercent = deltaY / 512.0;
+
+  // int motorSpeed = 200;
+  // int xSteps = abs((int)(xPercent*motorSpeed));
+  // int ySteps = abs((int)(yPercent*motorSpeed));
+
+  if (!singleStep)
+  {
+    int motorSpeedDelay = 500;
+    float xSpeed = motorSpeedDelay + motorSpeedDelay * (1 - xPercent);
+    float ySpeed = motorSpeedDelay + motorSpeedDelay * (1 - yPercent);
+
+    if (deltaX >= 0)
+    {
+      digitalWrite(xDirPin, clockwise);
+    }
+    else
+    {
+      digitalWrite(xDirPin, counterClockwise);
+    }
+    if (deltaY >= 0)
+    {
+      digitalWrite(yDirPin, clockwise);
+    }
+    else
+    {
+      digitalWrite(yDirPin, counterClockwise);
     }
 
-    if (singleStep) {
-        digitalWrite(statusLight, HIGH);
-    } else {
-        digitalWrite(statusLight, LOW);
+    if (deltaX < -20 || deltaX > 20)
+    {
+
+      digitalWrite(xStepPin, HIGH); // sets the digital pin 13 on
+      delayMicroseconds(xSpeed);
+      digitalWrite(xStepPin, LOW); // sets the digital pin 13 on
+      delayMicroseconds(xSpeed);
+      // Serial.println(xSpeed);
     }
 
-    // print data to Serial Monitor on Arduino IDE
-    // Serial.print("x = ");
-    // Serial.print(xValue);
-    // Serial.print(", y = ");
-    // Serial.println(yValue);
-    //delay(200);
+    // Serial.println("X Stepped" + xSteps);
+    if (deltaY < -20 || deltaY > 20)
+    {
 
-    int deltaX = xValue -512;
-    int deltaY = yValue -512;
-
-
-    float xPercent = deltaX/512.0;
-    float yPercent = deltaY/512.0;
-
-    // int motorSpeed = 200;
-    // int xSteps = abs((int)(xPercent*motorSpeed));
-    // int ySteps = abs((int)(yPercent*motorSpeed));
-
-    if (!singleStep) {
-        int motorSpeedDelay = 500;
-        float xSpeed = motorSpeedDelay + motorSpeedDelay *(1-xPercent);
-        float ySpeed = motorSpeedDelay + motorSpeedDelay *(1-yPercent);
-
-
-        if(deltaX >=0){
-            digitalWrite(xDirPin,clockwise);
-        }else{
-            digitalWrite(xDirPin,counterClockwise);
-        }
-        if(deltaY >=0){
-            digitalWrite(yDirPin,clockwise);
-        }else{
-            digitalWrite(yDirPin,counterClockwise);
-        }
-
-
-        if(deltaX<-20 || deltaX >20){
-            
-            digitalWrite(xStepPin, HIGH); // sets the digital pin 13 on
-            delayMicroseconds(xSpeed);   
-            digitalWrite(xStepPin, LOW); // sets the digital pin 13 on
-            delayMicroseconds(xSpeed);  
-            //Serial.println(xSpeed);
-        }
-
-
-        // Serial.println("X Stepped" + xSteps);
-        if(deltaY<-20 || deltaY >20){
-        
-            digitalWrite(yStepPin, HIGH); // sets the digital pin 13 off
-            delayMicroseconds(ySpeed); // waits for a milisecond
-            digitalWrite(yStepPin, LOW); // sets the digital pin 13 off
-            delayMicroseconds(ySpeed); // waits for a milisecond
-            //Serial.println(ySpeed);
-            
-        }
-    } else {
-        if(deltaX >=0){
-            digitalWrite(xDirPin,clockwise);
-        }else{
-            digitalWrite(xDirPin,counterClockwise);
-        }
-        if(deltaY >=0){
-            digitalWrite(yDirPin,clockwise);
-        }else{
-            digitalWrite(yDirPin,counterClockwise);
-        }
-
-        if(deltaX<-256 || deltaX >256){
-            step = true;
-        Serial.println("step");
-            digitalWrite(xStepPin, HIGH); // sets the digital pin 13 on
-            delayMicroseconds(1000);   
-            digitalWrite(xStepPin, LOW); // sets the digital pin 13 on
-            delayMicroseconds(1000);
-        }
-
-
-        // Serial.println("X Stepped" + xSteps);
-        if((deltaY<-256 || deltaY >256)){
-            step = true;
-            digitalWrite(yStepPin, HIGH); // sets the digital pin 13 off
-            delayMicroseconds(1000); // waits for a milisecond
-            digitalWrite(yStepPin, LOW); // sets the digital pin 13 off
-            delayMicroseconds(1000); // waits for a milisecond
-            //Serial.println(ySpeed);
-            
-        }
-        while(step && ((xValue < 412 || xValue > 612) || (yValue < 412 || yValue > 612 ))) {
-            xValue = analogRead(VRX_PIN);
-            yValue = analogRead(VRY_PIN);
-        }
-        step = false;
+      digitalWrite(yStepPin, HIGH); // sets the digital pin 13 off
+      delayMicroseconds(ySpeed);    // waits for a milisecond
+      digitalWrite(yStepPin, LOW);  // sets the digital pin 13 off
+      delayMicroseconds(ySpeed);    // waits for a milisecond
+                                    // Serial.println(ySpeed);
     }
-    
-    //  Serial.println("Y Stepped"+ ySteps);
+  }
+  else
+  {
+    if (deltaX >= 0)
+    {
+      digitalWrite(xDirPin, clockwise);
+    }
+    else
+    {
+      digitalWrite(xDirPin, counterClockwise);
+    }
+    if (deltaY >= 0)
+    {
+      digitalWrite(yDirPin, clockwise);
+    }
+    else
+    {
+      digitalWrite(yDirPin, counterClockwise);
+    }
 
+    if (deltaX < -256 || deltaX > 256)
+    {
+      step = true;
+      Serial.println("step x");
+      digitalWrite(xStepPin, HIGH); // sets the digital pin 13 on
+      delayMicroseconds(1000);
+      digitalWrite(xStepPin, LOW); // sets the digital pin 13 on
+      delayMicroseconds(1000);
+    }
+
+    // Serial.println("X Stepped" + xSteps);
+    if ((deltaY < -256 || deltaY > 256))
+    {
+      step = true;
+      Serial.println("step y");
+      digitalWrite(yStepPin, HIGH); // sets the digital pin 13 off
+      delayMicroseconds(1000);      // waits for a milisecond
+      digitalWrite(yStepPin, LOW);  // sets the digital pin 13 off
+      delayMicroseconds(1000);      // waits for a milisecond
+                                    // Serial.println(ySpeed);
+    }
+    // require joystick to be reset to position to 412 <= x, y <= 612
+    while (step && ((xValue < 412 || xValue > 612) || (yValue < 412 || yValue > 612)))
+    {
+      xValue = analogRead(VRX_PIN);
+      yValue = analogRead(VRY_PIN);
+    }
+    step = false;
+  }
+
+  //  Serial.println("Y Stepped"+ ySteps);
 }
-
-
 
 /*
-*KIND OF BUGGY FIRST IMPLEMENTATION;
-*DOESNT STEP HOW YOU WANT.
-*/
+ *KIND OF BUGGY FIRST IMPLEMENTATION;
+ *DOESNT STEP HOW YOU WANT.
+ */
 
 // #define VRX_PIN  A0 // Arduino pin connected to VRX pin
 // #define VRY_PIN  A1 // Arduino pin connected to VRY pin
@@ -174,7 +194,6 @@ void loop() {
 
 // #define counterClockwise HIGH
 // #define clockwise LOW
-
 
 // int xValue = 0; // To store value of the X axis
 // int yValue = 0; // To store value of the Y axis
@@ -205,7 +224,6 @@ void loop() {
 //     int deltaX = xValue -512;
 //     int deltaY = yValue -512;
 
-
 //     float xPercent = deltaX/512.0;
 //     float yPercent = deltaY/512.0;
 
@@ -227,9 +245,9 @@ void loop() {
 //     if(deltaX<20 || deltaX >20){
 //         for(int x = 0; x<xSteps; x++){
 //             digitalWrite(xStepPin, HIGH); // sets the digital pin 13 on
-//             delayMicroseconds(1000);   
+//             delayMicroseconds(1000);
 //             digitalWrite(xStepPin, LOW); // sets the digital pin 13 on
-//             delayMicroseconds(1000);  
+//             delayMicroseconds(1000);
 //         }
 //     }
 
@@ -243,17 +261,11 @@ void loop() {
 //             delayMicroseconds(1000); // waits for a milisecond
 //         }
 //     }
-    
+
 //     //  Serial.println("Y Stepped"+ ySteps);
 //      Serial.println(ySteps);
 
 // }
-
-
-
-
-
-
 
 /*
  * Created by ArduinoGetStarted.com
@@ -287,8 +299,8 @@ void loop() {
 // }
 
 /*
-*BLINK 
-*/
+ *BLINK
+ */
 // void setup()
 // {
 //     pinMode(13, OUTPUT); // sets the digital pin 13 as output
@@ -303,8 +315,8 @@ void loop() {
 // }
 
 /*
-*Main Program
-*/
+ *Main Program
+ */
 // #define xStepPin 2
 // #define xDirPin 5
 // #define status 6
